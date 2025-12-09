c1b5fcc945ebe0dd7da6098be6ef89714621e526
Ashley <156200635+BlahajStealer@users.noreply.github.com>
2025-12-09 14:13:51 -0500

    Haven't done much these last few days.
    All I've really done is made Buttons work, they have an ID number sent to a seperate script which can then be implemented later. Random numbers displayed, which will later be replaced with sprites.

--- Assets/ShopScript.cs
+++ Assets/ShopScript.cs
@@ -5,6 +5,7 @@ using static UnityEngine.Random;
 
 public class ShopScript : MonoBehaviour
 {
+    public idDeterminer IdDet;
     public float Money;
     public TextMeshProUGUI MoneyText;
     public GameObject Univ;
@@ -17,29 +18,65 @@ public class ShopScript : MonoBehaviour
     public Button[] MachineModsButton;
     public bool shopMoneyStart;
     public bool shopMoneyStarted;
-
+    public int[] BadgeArray;
+    public int[] BallArray;
+    public int[] MachineArray;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        IdDet = GetComponent<idDeterminer>();
         us = Univ.GetComponent<UniversalScript>();
-        
+
+        BadgeArray = new int[BadgeButtons.Length];
+        BallArray = new int[BallButtons.Length];
+        MachineArray = new int[MachineModsButton.Length];
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < Badges.Length; i++)
-        {
-            BadgeButtons[i].image.sprite = Badges[Random.Range(0,Badges.Length)];
-        }
+
         if (shopMoneyStart && !shopMoneyStarted)
         {
             Money += us.score/100;
             shopMoneyStart = false;
             shopMoneyStarted = true;
+            for (int i = 0; i < BadgeButtons.Length; i++)
+            {
+                int RandomInt = Random.Range(0,Badges.Length);
+                BadgeButtons[i].image.sprite = Badges[RandomInt];
+                BadgeArray[i] = RandomInt;
+            }
+            for (int i = 0; i < BallButtons.Length; i++)
+            {
+                int RandomInt = Random.Range(0,BallMods.Length);
+                BallButtons[i].image.sprite = BallMods[RandomInt];
+                BallArray[i] = RandomInt;
+            }
+            for (int i = 0; i < MachineModsButton.Length; i++)
+            {
+                int RandomInt = Random.Range(0,MachineMods.Length);
+                MachineModsButton[i].image.sprite = MachineMods[RandomInt];
+                MachineArray[i] = RandomInt;
+            }
         }
         MoneyText.text = "Money: " + Money.ToString();
 
     }
+    public void BadgeButtonHit(int ID)
+    {
+        IdDet.Badge(BadgeArray[ID]);
+    }
+    public void BallButtonHit(int ID)
+    {
+        IdDet.Ball(BallArray[ID]);
+    }
+    public void MachineButtonHit(int ID)
+    {
+        IdDet.Machine(MachineArray[ID]);
+    }
 }
+
+
---
+++ Assets/idDeterminer.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class idDeterminer : MonoBehaviour
+{
+    public void Badge(int ID)
+    {
+        switch (ID)
+        {
+            case 0: 
+                Debug.Log("One is Displayed");
+                break;
+            case 1: 
+                Debug.Log("Two is Displayed");
+                break;
+            case 2: 
+                Debug.Log("Three is Displayed");
+                break;
+            case 3: 
+                Debug.Log("Four is Displayed");
+                break;
+            case 4: 
+                Debug.Log("Five is Displayed");
+                break;
+            case 5: 
+                Debug.Log("Six is Displayed");
+                break;
+            default:
+                Debug.Log("How did you get here: Acheivment Unlocked");
+                break;
+        }
+    }
+    public void Ball(int ID)
+    {
+        switch (ID)
+        {
+            case 0: 
+                Debug.Log("One is Displayed");
+                break;
+            case 1: 
+                Debug.Log("Two is Displayed");
+                break;
+            case 2: 
+                Debug.Log("Three is Displayed");
+                break;
+            case 3: 
+                Debug.Log("Four is Displayed");
+                break;
+            case 4: 
+                Debug.Log("Five is Displayed");
+                break;
+            case 5: 
+                Debug.Log("Six is Displayed");
+                break;
+            default:
+                Debug.Log("How did you get here: Acheivment Unlocked");
+                break;
+        }
+    }
+    public void Machine(int ID)
+    {
+        switch (ID)
+        {
+            case 0: 
+                Debug.Log("One is Displayed");
+                break;
+            case 1: 
+                Debug.Log("Two is Displayed");
+                break;
+            case 2: 
+                Debug.Log("Three is Displayed");
+                break;
+            case 3: 
+                Debug.Log("Four is Displayed");
+                break;
+            case 4: 
+                Debug.Log("Five is Displayed");
+                break;
+            case 5: 
+                Debug.Log("Six is Displayed");
+                break;
+            default:
+                Debug.Log("How did you get here: Acheivment Unlocked");
+                break;
+        }
+    }
+
+}
