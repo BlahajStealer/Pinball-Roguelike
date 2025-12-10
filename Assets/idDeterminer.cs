9576f1f5fd75d8f02d916d5ba9562faa4df3f202
Ashley <156200635+BlahajStealer@users.noreply.github.com>
2025-12-10 10:46:26 -0500

    Money Changes
    Made it so money is lost when clicking on a button, Ball Mods doesnt work for some reason

--- Assets/idDeterminer.cs
+++ Assets/idDeterminer.cs
@@ -2,30 +2,76 @@ using UnityEngine;
 
 public class idDeterminer : MonoBehaviour
 {
+
+    ShopScript ss;
+    int MoneyTaken;
+    void Start()
+    {
+        ss = GetComponent<ShopScript>();
+    }
     public void Badge(int ID)
     {
         switch (ID)
         {
             case 0: 
+                MoneyTaken = 10;
                 Debug.Log("One is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 1: 
+                MoneyTaken = 20;
+
                 Debug.Log("Two is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 2: 
+                MoneyTaken = 30;
                 Debug.Log("Three is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 3: 
+                MoneyTaken = 40;
                 Debug.Log("Four is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 4: 
+                MoneyTaken = 50;
                 Debug.Log("Five is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 5: 
+                MoneyTaken = 60;
                 Debug.Log("Six is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             default:
                 Debug.Log("How did you get here: Acheivment Unlocked");
+                ss.Money -= 1000;
+
                 break;
         }
     }
@@ -34,22 +80,61 @@ public class idDeterminer : MonoBehaviour
         switch (ID)
         {
             case 0: 
+                MoneyTaken = 10;
+
                 Debug.Log("One is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }                
                 break;
             case 1: 
+                MoneyTaken = 20;
+
                 Debug.Log("Two is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 2: 
+                MoneyTaken = 30;
+
                 Debug.Log("Three is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 3: 
+                MoneyTaken = 40;
                 Debug.Log("Four is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 4: 
+                MoneyTaken = 50;
                 Debug.Log("Five is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 5: 
+                MoneyTaken = 60;
                 Debug.Log("Six is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             default:
                 Debug.Log("How did you get here: Acheivment Unlocked");
@@ -61,25 +146,66 @@ public class idDeterminer : MonoBehaviour
         switch (ID)
         {
             case 0: 
+                MoneyTaken = 10;
+
                 Debug.Log("One is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 1: 
+                MoneyTaken = 20;
+
                 Debug.Log("Two is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 2: 
+                MoneyTaken = 30;
+
                 Debug.Log("Three is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 3: 
+                MoneyTaken = 40;
                 Debug.Log("Four is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 4: 
+                MoneyTaken = 50;
                 Debug.Log("Five is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             case 5: 
+                MoneyTaken = 60;
                 Debug.Log("Six is Displayed");
+                if (MoneyTaken <= ss.Money)
+                {
+                    ss.Money -= MoneyTaken;
+                    break;
+                }
                 break;
             default:
                 Debug.Log("How did you get here: Acheivment Unlocked");
+                ss.Money -= 10;
+
                 break;
         }
     }
